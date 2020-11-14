8de9c4933f9411677037de129cc5dc7a0ae2f6cb
Kamil Zawistowski <kamil.zawistowski@gds.ey.com>
2020-11-14 15:35:33 +0100

    get shot level labels as DataFrame

--- second-task/models/video_to_labels.py
+++ second-task/models/video_to_labels.py
@@ -1,57 +1,33 @@
-from google.cloud import videointelligence
-from google.oauth2 import service_account
 import io
 
-def process(filename):
+import pandas as pd
+from google.cloud import videointelligence
+from google.oauth2 import service_account
+
+
+def process(video_path: str) -> pd.DataFrame:
     my_credentials = service_account.Credentials.from_service_account_file('apikey.json')
     video_client = videointelligence.VideoIntelligenceServiceClient(credentials=my_credentials)
     features = [videointelligence.enums.Feature.LABEL_DETECTION]
 
-    with io.open(f'input_vid/{filename}', "rb") as movie:
+    with io.open(video_path, "rb") as movie:
         input_content = movie.read()
 
     operation = video_client.annotate_video(
         features=features, input_content=input_content
     )
-    print("\nProcessing video for label annotations:")
-
     result = operation.result(timeout=90)
-    print("\nFinished processing.")
 
-    # Process video/segment level label annotations
-    segment_labels = result.annotation_results[0].segment_label_annotations
-    for i, segment_label in enumerate(segment_labels):
-        print("Video label description: {}".format(segment_label.entity.description))
-        for category_entity in segment_label.category_entities:
-            print(
-                "\tLabel category description: {}".format(category_entity.description)
-            )
-
-        for i, segment in enumerate(segment_label.segments):
-            start_time = (
-                segment.segment.start_time_offset.seconds
-                + segment.segment.start_time_offset.nanos / 1e9
-            )
-            end_time = (
-                segment.segment.end_time_offset.seconds
-                + segment.segment.end_time_offset.nanos / 1e9
-            )
-            positions = "{}s to {}s".format(start_time, end_time)
-            confidence = segment.confidence
-            print("\tSegment {}: {}".format(i, positions))
-            print("\tConfidence: {}".format(confidence))
-        print("\n")
+    results = pd.DataFrame(columns=['label', 'category', 'start_time', 'end_time', 'confidence'])
 
     # Process shot level label annotations
     shot_labels = result.annotation_results[0].shot_label_annotations
     for i, shot_label in enumerate(shot_labels):
-        print("Shot label description: {}".format(shot_label.entity.description))
-        for category_entity in shot_label.category_entities:
-            print(
-                "\tLabel category description: {}".format(category_entity.description)
-            )
+        label = shot_label.entity.description
 
-        for i, shot in enumerate(shot_label.segments):
+        category = shot_label.category_entities[0].description if shot_label.category_entities else None
+
+        for shot in shot_label.segments:
             start_time = (
                 shot.segment.start_time_offset.seconds
                 + shot.segment.start_time_offset.nanos / 1e9
@@ -60,31 +36,20 @@ def process(filename):
                 shot.segment.end_time_offset.seconds
                 + shot.segment.end_time_offset.nanos / 1e9
             )
-            positions = "{}s to {}s".format(start_time, end_time)
             confidence = shot.confidence
-            print("\tSegment {}: {}".format(i, positions))
-            print("\tConfidence: {}".format(confidence))
-        print("\n")
-
-    # Process frame level label annotations
-    frame_labels = result.annotation_results[0].frame_label_annotations
-    for i, frame_label in enumerate(frame_labels):
-        print("Frame label description: {}".format(frame_label.entity.description))
-        for category_entity in frame_label.category_entities:
-            print(
-                "\tLabel category description: {}".format(category_entity.description)
+            results = results.append(
+                {
+                    'label': label,
+                    'category': category,
+                    'start_time': start_time,
+                    'end_time': end_time,
+                    'confidence': confidence
+                },
+                ignore_index=True
             )
 
-        # Each frame_label_annotation has many frames,
-        # here we print information only about the first frame.
-        frame = frame_label.frames[0]
-        time_offset = frame.time_offset.seconds + frame.time_offset.nanos / 1e9
-        print("\tFirst frame time offset: {}s".format(time_offset))
-        print("\tFirst frame confidence: {}".format(frame.confidence))
-        print("\n")
-
-    return shot_labels
+    return results
 
 
 if __name__ == '__main__':
-    process("short.mp4")
+    process("/Users/ag283qj/code/Skyhack-3/Jesteśmy zgubieni.mp4")
