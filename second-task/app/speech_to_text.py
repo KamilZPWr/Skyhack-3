3a49808bb893126e90d11e4055fa2fa7a8c84db3
Kamil Zawistowski <kamil.zawistowski@gds.ey.com>
2020-11-14 08:44:38 +0100

    add req versions, provide smaller chunks and time indexing

--- second-task/app/speech_to_text.py
+++ second-task/app/speech_to_text.py
@@ -1,39 +1,43 @@
-from pydub import AudioSegment  # sudo apt-get install ffmpeg
-import speech_recognition as sr
 import json
-import os
+
+import speech_recognition as sr
+from pydub import AudioSegment  # sudo apt-get install ffmpeg
 
 
-def process(filename, chunksize=60000):
-    sound = AudioSegment.from_mp3(f'input_audio/{filename}')
+def process(filename, chunksize=5000):
+    sound = AudioSegment.from_mp3(filename)
     # split
 
     def divide_chunks(sound, chunksize):
         for i in range(0, len(sound), chunksize):
             yield sound[i:i + chunksize]
+
     chunks = list(divide_chunks(sound, chunksize))
     r = sr.Recognizer()
-    joined_text = ''
+    results = []
     # convert to wav
     for index, chunk in enumerate(chunks):
-        chunk.export(f'output_audio/{filename}.wav', format='wav')
+        chunk.export(f'{filename}.wav', format='wav')
         # recognize
-        with sr.AudioFile(f'output_audio/{filename}.wav') as source:
+        with sr.AudioFile(f'{filename}.wav') as source:
             audio = r.record(source)
         try:
             with open("apikey.json", ) as f:
                 apikey = json.load(f)
                 apikey = json.dumps(apikey)
             s = r.recognize_google_cloud(audio, language="pl-PL", credentials_json=apikey)
+            results.append((index*5, s))
         except:
             # Darmowa metoda, można sprawdzić do 60 requestów jakby cos z kluczem do API nie dzialalo
             s = r.recognize_google(audio, language="pl-PL")
-        joined_text += f'{s} '
-    return joined_text
+            results.append((index*5, s))
+
+    return results
 
 
 if __name__ == "__main__":
-    for filename in os.listdir('input_audio'):
-        text = process(filename)
+    for filename in ['/Users/ag283qj/code/Skyhack-3/second-task/storage/Jesteśmy zgubieni.mp3']:
+        results = process(filename)
+        text = ' '.join([text for _, text in results])
         print(f'Nazwa pliku: {filename}')
         print(f'Treść: {text}')
