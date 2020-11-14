7037afd9bb7ffcee6bad2ff3d2cea54842d594a7
Łukasz Cupek <cupek@icloud.com>
2020-11-14 01:22:30 +0100

    Files chunking, covnersion and recognition by chunks.

--- second-task/app/speech_to_text.py
+++ second-task/app/speech_to_text.py
@@ -1,21 +1,44 @@
-import requests
+from pydub import AudioSegment # sudo apt-get install ffmpeg
+import speech_recognition as sr
 import json
-from csv import reader
-import io
-import os
-from google.cloud import proto
-from google.oauth2 import service_account # Instantiates a client
-my_credentials = service_account.Credentials.from_service_account_file('apikey.json')
-client = proto.speech.SpeechClient(credentials=my_credentials)
-config = proto.speech.RecognitionConfig(
-    encoding=proto.speech.RecognitionConfig.AudioEncoding.MP3,
-    sample_rate_hertz=44100,
-    language_code="pl-PL",
-)
-storage_uri = "gs://audio_skyhacks/710458.mp3"
-audio = {"uri": storage_uri}
-operation = client.long_running_recognize(config=config, audio=audio)
-response = operation.result()
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
 
+from pathlib import Path
+#from pydub.silence import split_on_silence
+#import io
+#from pocketsphinx import AudioFile, Pocketsphinx
+
+# Opening JSON file
+with open("apikey.json", ) as f:
+    apikey = json.load(f)
+    apikey = json.dumps(apikey)
+
+def process(filepath, chunksize=60000):
+    #0: load mp3
+    sound = AudioSegment.from_mp3(filepath)
+
+    #1: split file into 60s chunks
+    def divide_chunks(sound, chunksize):
+        # looping till length l
+        for i in range(0, len(sound), chunksize):
+            yield sound[i:i + chunksize]
+    chunks = list(divide_chunks(sound, chunksize))
+    print(f"{len(chunks)} chunks of {chunksize/1000}s each")
+
+    r = sr.Recognizer()
+    #2: per chunk, save to wav, then read and run through recognize_google()
+    string_index = {}
+    for index,chunk in enumerate(chunks):
+        #TODO io.BytesIO()
+        chunk.export(f'output_audio/{filepath}.wav', format='wav')
+        with sr.AudioFile(f'output_audio/{filepath}.wav') as source:
+            audio = r.record(source)
+        # Darmowa metoda
+        # s = r.recognize_google(audio, language="pl-PL")
+
+        s = r.recognize_google_cloud(audio, language="pl-PL", credentials_json=apikey)
+        print(s)
+        string_index[index] = s
+        break
+    return string_index
+
+text = process('710458.mp3')
