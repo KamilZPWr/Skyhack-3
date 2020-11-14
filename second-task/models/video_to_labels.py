a9a6a9fa341739a5d3e7fbc6ac4ff7d725a9bf51
Kamil Zawistowski <kamil.zawistowski@gds.ey.com>
2020-11-14 23:02:39 +0100

    integrate video to labels with webapp

--- second-task/models/video_to_labels.py
+++ second-task/models/video_to_labels.py
@@ -1,10 +1,15 @@
 import io
 
+import cv2
 import pandas as pd
 from google.cloud import videointelligence
+from google.oauth2 import service_account
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+from models.labels_matcher import EXPECTED_LABELS, get_similar_label
 
 
-def process(video_path: str, credentials) -> pd.DataFrame:
+def extract_labels(video_path: str, credentials) -> pd.DataFrame:
     video_client = videointelligence.VideoIntelligenceServiceClient(credentials=credentials)
     features = [videointelligence.enums.Feature.LABEL_DETECTION]
 
@@ -16,7 +21,7 @@ def process(video_path: str, credentials) -> pd.DataFrame:
     )
     result = operation.result(timeout=90)
 
-    results = pd.DataFrame(columns=['label', 'category', 'start_time', 'end_time', 'confidence'])
+    results = pd.DataFrame(columns=['label', 'category', 'start_time', 'end_time'])
 
     # Process shot level label annotations
     shot_labels = result.annotation_results[0].shot_label_annotations
@@ -34,14 +39,12 @@ def process(video_path: str, credentials) -> pd.DataFrame:
                 shot.segment.end_time_offset.seconds
                 + shot.segment.end_time_offset.nanos / 1e9
             )
-            confidence = shot.confidence
             results = results.append(
                 {
                     'label': label,
                     'category': category,
                     'start_time': start_time,
                     'end_time': end_time,
-                    'confidence': confidence
                 },
                 ignore_index=True
             )
@@ -49,5 +52,43 @@ def process(video_path: str, credentials) -> pd.DataFrame:
     return results
 
 
+def decrease_fps(video_path):
+    new_path = video_path.replace('.mp4', '_5fps.mp4')
+    clip = VideoFileClip(video_path)
+    clip.write_videofile(new_path, fps=5)
+    clip.reader.close()
+    return new_path
+
+
+def process(video_path: str, credentials):
+    from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+
+    ffmpeg_extract_subclip(video_path, 0, 30, targetname=video_path.replace('.mp4', '_cuted.mp4'))
+    video_path = video_path.replace('.mp4', '_cuted.mp4')
+    video_path = decrease_fps(video_path)
+
+    video = cv2.VideoCapture(video_path)
+    video_seconds = int(video.get(cv2.CAP_PROP_FRAME_COUNT)/video.get(cv2.CAP_PROP_FPS))
+    extracted_labels = extract_labels(video_path, credentials)
+
+    results = pd.DataFrame(data={'second': list(range(1, video_seconds))})
+    results[EXPECTED_LABELS] = 0
+
+    for _, row in extracted_labels.iterrows():
+        label = row['label']
+        assigned_labels = get_similar_label(label, 0.55) if label else None
+
+        for assigned_label in assigned_labels:
+            results.loc[
+                (row['start_time'] < results['second']) & (results['second'] <= row['end_time']),
+                assigned_label
+            ] = 1
+
+    return results
+
+
 if __name__ == '__main__':
-    process("/Users/ag283qj/code/Skyhack-3/Jesteśmy zgubieni.mp4")
+    credentials = service_account.Credentials.from_service_account_file('/Users/ag283qj/code/Skyhack-3/apikey.json')
+    k = process('/Users/ag283qj/code/Skyhack-3/20190829 zamek_ogrodzieniec_360_FINALNA_audiodeskrypcja_5fps.mp4', credentials)
+    k.to_csv('inny.csv', index=False)
+    p = 1
