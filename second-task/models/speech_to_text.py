3924ad5150d4da6e76f97a2c5f360d28167bfb5a
Kamil Zawistowski <kamil.zawistowski@gds.ey.com>
2020-11-15 04:56:31 +0100

    integrate chunk audio with labels assigment and webapp

--- second-task/models/speech_to_text.py
+++ second-task/models/speech_to_text.py
@@ -1,8 +1,10 @@
-import json
 from typing import List, Tuple
 
+import pandas as pd
 import speech_recognition as sr
-from pydub import AudioSegment  #
+from pydub import AudioSegment
+
+from models.labels_matcher import get_similar_label, EXPECTED_LABELS
 
 
 def divide_chunks(sound, chunk_size):
@@ -10,9 +12,10 @@ def divide_chunks(sound, chunk_size):
         yield sound[i:i + chunk_size]
 
 
-def process(file_path: str, chunk_size: int = 5000, apikey=None) -> List[Tuple[int, str]]:
+def process(file_path: str, chunk_size: int = 60000, apikey=None) -> Tuple[List[Tuple[int, str]], int]:
 
     sound = AudioSegment.from_mp3(file_path)
+    seconds = sound.duration_seconds
     chunks = list(divide_chunks(sound, chunk_size))
 
     r = sr.Recognizer()
@@ -24,12 +27,29 @@ def process(file_path: str, chunk_size: int = 5000, apikey=None) -> List[Tuple[i
 
         with sr.AudioFile(wav_file_path) as source:
             audio = r.record(source)
+
         if apikey:
             s = r.recognize_google_cloud(audio, language="pl-PL", credentials_json=apikey)
-            results.append((index*5, s))
+            words_per_second = len(s.split()) / 60
+            word_counter = 1
+            for word in s.split():
+                results.append((int((index * 60) + word_counter / words_per_second), word))
+                word_counter += 1
+            print(index)
         else:
             s = r.recognize_google(audio, language="pl-PL")
             results.append((index*5, s))
+
+    return results, seconds
+
+
+def assign_label(chunks: List[Tuple[int, str]], max_second) -> pd.DataFrame:
+    results = pd.DataFrame(data={'second': list(range(1, int(max_second)))})
+    results[EXPECTED_LABELS] = 0
+
+    for chunk in chunks:
+        labels = get_similar_label(chunk[1])
+        results.loc[results['second'] == chunk[0], labels] = 1
 
     return results
 
--- second-task/models/translator.py
+++ second-task/models/translator.py
@@ -1,26 +1,25 @@
+from typing import List, Tuple
+
 from google.cloud import translate_v2
 from google.oauth2 import service_account
 
 
-def translate(text):
-    my_credentials = service_account.Credentials.from_service_account_file('apikey.json')
-    translate_client = translate_v2.Client(credentials=my_credentials)
+def translate(text, credentials):
+    translate_client = translate_v2.Client(credentials=credentials)
     result = translate_client.translate(text, target_language='en', source_language='pl')
-
-    # print(u"Text: {}".format(result["input"]))
-    # print(u"Translation: {}".format(result["translatedText"]))
     return result["translatedText"]
 
 
-def translate_list_of_chunks(chunks: list) -> list:
-    """ Accepts list of tuples like [(int, string), ...]"""
+def translate_list_of_chunks(chunks: List[Tuple[int, str]], apikey_path: str) -> List[Tuple[int, str]]:
     result_list = []
+    credentials = service_account.Credentials.from_service_account_file(apikey_path)
+
     for chunk in chunks:
-        translated = translate(chunk[1])
+        translated = translate(chunk[1], credentials)
         result_list.append((chunk[0], translated))
     return result_list
 
 
 if __name__ == '__main__':
     chunks_list = [(0, "Wszedł kotek na płotek. Lubię placki."), (1, "inny język"), (2, 'ostatnia partia')]
-    translate_list_of_chunks(chunks_list)
+    translate_list_of_chunks(chunks_list, 'apikey.json')
